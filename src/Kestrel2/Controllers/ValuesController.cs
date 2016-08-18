9245ad9c1368717eb6cf1eb284d20b75192ac2fb
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2016-08-18 17:10:38 +0300

    init

---
+++ src/Kestrel2/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kestrel2.Controllers
+{
+    public class Person
+    {
+        public readonly string Name;
+        public readonly int Age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+
+    [Route("/[controller]")]
+    public class PersonController : Controller
+    {
+        // GET api/values
+        //[HttpGet]
+        //public string Get() => "value1";
+
+        // GET api/values/5
+        [HttpGet("{name}")]
+        public Person Get(string name) => new Person(name, 41);
+
+        // POST api/values
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        // PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        // DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ src/Kestrel2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+
+namespace Kestrel2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseUrls("http://0.0.0.0:5000")
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
